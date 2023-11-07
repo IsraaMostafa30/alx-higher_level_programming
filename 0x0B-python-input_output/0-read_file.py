96d8421b2ce6f451caa18c04cecb81f662c095bd
Israa Mostafa <mustafa.esraa@yahoo.com>
2023-11-07 14:17:46 -0800

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
+"""this for Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
+    """for Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
